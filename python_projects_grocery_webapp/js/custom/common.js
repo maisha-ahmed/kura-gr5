d8b4ee8520dbb18845d443c5b2f14e319f3f77a6
Levy Andrew <85814374+levyandrew16@users.noreply.github.com>
2021-12-21 12:31:52 -0500

    Add files via upload

--- python_projects_grocery_webapp/js/custom/common.js
+++ python_projects_grocery_webapp/js/custom/common.js
@@ -1,10 +1,10 @@
 // Define your api here
-var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
-var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
-var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
-var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
-var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
-var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
+var productListApiUrl = 'http://18.117.167.182:5000/getProducts';
+var uomListApiUrl = 'http://18.117.167.182:5000/getUOM';
+var productSaveApiUrl = 'http://18.117.167.182:5000/insertProduct';
+var productDeleteApiUrl = 'http://18.117.167.182:5000/deleteProduct';
+var orderListApiUrl = 'http://18.117.167.182:5000/getAllOrders';
+var orderSaveApiUrl = 'http://18.117.167.182:5000/insertOrder';
 
 // For product drop in order
 var productsApiUrl = 'https://fakestoreapi.com/products';
