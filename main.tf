f1e11ece93a676492b10a6c6ae43962f260ef90d
unknown <jespstpierre@yahoo.com>
2022-01-19 16:29:11 -0500

    set up8

--- main.tf
+++ main.tf
@@ -105,3 +105,50 @@ resource "aws_subnet" "private-subnet-1b" {
 }
 
 
+# Create Web layber route table
+# web and app are used interchangeably
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "WebRT"
+    "Terraform" = true
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.pub-subnet-1a.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.pub-subnet-1b.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+# Private route table
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT.id
+    }
+    tags = {
+      "Name" = "prvt-rt-tbl"
+      "Terraform" = true
+    }
+}
+
+# Associate Private Route Table to 
+# Private Subnet = Private-1a
+resource "aws_route_table_association" "private-rt" {
+  subnet_id = aws_subnet.application-subnet-1a.id
+  route_table_id = aws_route_table.private-route-table.id
+
+}
+
