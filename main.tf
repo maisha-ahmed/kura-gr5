d8edb7bf5e3f09e6fc22dbdfd827ff6c63b9faef
unknown <jespstpierre@yahoo.com>
2022-01-18 20:36:30 -0500

    vpc set-up

--- main.tf
+++ main.tf
@@ -17,11 +17,61 @@ terraform {
   }
 }
 
+#VPC cidr block 
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "Main VPC"
     }
 }
+
+
+# 4 public subnets
+resource "aws_subnet" "pub-subnet-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-1a"
+  }
+}
+
+resource "aws_subnet" "pub-subnet-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-1b"
+  }
+}
+
+
+# Database Private Subnet
+resource "aws_subnet" "private-subnet-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Database-1a"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Database-1b"
+  }
+}
+
+
